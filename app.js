2cfe253fc1597af811ce43eec2b7e66847930528
mhcuser <mhcuser@n21-163.mtholyoke.edu>
2017-08-10 15:17:34 -0400

    finished quiz; button disappeared

--- app.js
+++ app.js
@@ -12,20 +12,21 @@ $(document).ready(function() {
     if (timesAButtonClicked > timesBButtonClicked) {
       // console.log("poop");
       $("#mickey").show();
+      $("#submit").hide();
     } else if (timesAButtonClicked < timesBButtonClicked) {
       $("#Olaf").show();
+      $("#submit").hide();
     }
   });
 
   $("#submit").click(function() {
     if (timesAButtonClicked > timesBButtonClicked) {
       $("#mickey").show();
+      $("#submit").hide();
       // console.log("poop");
     } else if (timesAButtonClicked < timesBButtonClicked) {
       $("#Olaf").show();
+      $("#submit").hide();
     }
-    // $("#submit").click(function() {
-    //   $("#submit").hide();
-    // });
   });
 });
